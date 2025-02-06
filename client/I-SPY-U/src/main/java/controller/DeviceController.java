2ba1b1fec347a2806092cb23bdd64194c1945b90
EinoRuuth <eino@ruuth.net>
2025-02-06 15:06:09 +0200

    added title to the winow and started on device functionality

--- client/I-SPY-U/src/main/java/controller/DeviceController.java
+++ client/I-SPY-U/src/main/java/controller/DeviceController.java
@@ -3,9 +3,17 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
 public class DeviceController {
+
+    private ToggleGroup toggleGroup = new ToggleGroup();
+
+    @FXML RadioButton radioDaily;
+    @FXML RadioButton radioWeekly;
+    @FXML RadioButton radioMontly;
 
     @FXML
     private void handleCloseButtonAction(ActionEvent event) {
@@ -15,6 +23,8 @@ public class DeviceController {
 
     @FXML
     private void initialize(){
-        
+        radioDaily.setToggleGroup(toggleGroup);
+        radioWeekly.setToggleGroup(toggleGroup);
+        radioMontly.setToggleGroup(toggleGroup);
     }
 }
