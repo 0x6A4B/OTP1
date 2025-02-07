f8e7d991e582b9bce19a9eb8c6629865ac535d3f
EinoRuuth <eino@ruuth.net>
2025-02-07 15:22:14 +0200

    populated device choiseBoxes and made main view look better

--- client/I-SPY-U/src/main/java/controller/DeviceController.java
+++ client/I-SPY-U/src/main/java/controller/DeviceController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 public class DeviceController {
@@ -14,6 +15,9 @@ public class DeviceController {
     @FXML RadioButton radioDaily;
     @FXML RadioButton radioWeekly;
     @FXML RadioButton radioMontly;
+
+    @FXML ChoiceBox<String> actionChoice;
+    @FXML ChoiceBox<String> shareChoice;
 
     @FXML
     private void handleCloseButtonAction(ActionEvent event) {
@@ -26,5 +30,8 @@ public class DeviceController {
         radioDaily.setToggleGroup(toggleGroup);
         radioWeekly.setToggleGroup(toggleGroup);
         radioMontly.setToggleGroup(toggleGroup);
+
+        actionChoice.setValue("Select an action");
+        shareChoice.setValue("Select a role");
     }
 }
