1aae804ad9b211f10effdae155c98d02cbd6439a
Niko <nikome@metropolia.fi>
2025-02-10 11:27:48 +0200

    Test for person, user, device models

---
+++ client/I-SPY-U/src/test/java/model/PersonTest.java
@@ -0,0 +1,95 @@
+package model;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    static Person person;
+
+    @BeforeEach
+    void init() {
+        person = new Person("Urho", "Kekkonen", "urho.kekkonen@email.fi",
+                            "Kekkosenkatu 12", "Kekkoslovakia", "2222");
+    }
+
+    @Test
+    void getId() {
+        assertEquals(0, person.getId());
+    }
+
+    @Test
+    void getFirstName() {
+        assertEquals("Urho", person.getFirstName());
+    }
+
+    @Test
+    void getLastName() {
+        assertEquals("Kekkonen", person.getLastName());
+    }
+
+    @Test
+    void getEmail() {
+        assertEquals("urho.kekkonen@email.fi", person.getEmail());
+    }
+
+    @Test
+    void getStreet() {
+        assertEquals("Kekkosenkatu 12", person.getStreet());
+    }
+
+    @Test
+    void getCity() {
+        assertEquals("Kekkoslovakia", person.getCity());
+    }
+
+    @Test
+    void getPostalCode() {
+        assertEquals("2222", person.getPostalCode());
+    }
+
+    @Test
+    void setId() {
+        person.setId(1L);
+        assertEquals(1, person.getId());
+    }
+
+    @Test
+    void setFirstName() {
+        person.setFirstName("Martti");
+        assertEquals("Martti", person.getFirstName());
+    }
+
+    @Test
+    void setLastName() {
+        person.setLastName("Ahtisaari");
+        assertEquals("Ahtisaari", person.getLastName());
+    }
+
+    @Test
+    void setEmail() {
+        person.setEmail("martti.ahtisaari@email.fi");
+        assertEquals("martti.ahtisaari@email.fi", person.getEmail());
+    }
+
+    @Test
+    void setStreet() {
+        person.setStreet("Ahtisaarentie 12");
+        assertEquals("Ahtisaarentie 12", person.getStreet());
+    }
+
+    @Test
+    void setCity() {
+        person.setCity("Ahtisaarilandia");
+        assertEquals("Ahtisaarilandia", person.getCity());
+    }
+
+    @Test
+    void setPostalCode() {
+        person.setPostalCode("1111");
+        assertEquals("1111", person.getPostalCode());
+    }
+}
