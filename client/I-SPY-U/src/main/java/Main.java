9832d77ac429cb383553b1ac70118bc2962109fc
0x6A4B <0x6a4b@proton.me>
2025-02-26 10:58:45 +0200

    httpquery started

--- client/I-SPY-U/src/main/java/Main.java
+++ client/I-SPY-U/src/main/java/Main.java
@@ -3,5 +3,10 @@ import view.GUI;
 public class Main {
     public static void main(String[] args) {
         GUI.launch(GUI.class);
+
+        //httpquery exmaple
+        //HttpQuery q = new HttpQuery("https://otp1.0x6a4b.dev/api");
+        //if (q.login())
+        //    q.getDev();
     }
 }
--- client/I-SPY-U/src/main/java/http/HttpQuery.java
+++ client/I-SPY-U/src/main/java/http/HttpQuery.java
@@ -1,17 +1,82 @@
 package http;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import org.json.JSONObject;
 
-public abstract class HttpQuery {
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+
+public /*abstract*/ class HttpQuery {
     private String apiUrl;
+    private String token = "";
+    private HttpClient httpClient = HttpClient.newHttpClient();
 
     public HttpQuery(String apiUrl) { this.apiUrl = apiUrl; }
 
     //public void setApiUrl(String apiUrl){ this.apiUrl = apiUrl; }
 
+    public boolean login(){
+        String endpoint = apiUrl + "/auth/login";
+        String body = "{\"username\":\"wasdi\", \"password\":\"wasdi\"}";
 
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endpoint))
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .setHeader("Content-Type", "application/json")
+                .build();
+
+        CompletableFuture<HttpResponse<String>> futureResponse
+                = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+        try {
+            HttpResponse<String> response = futureResponse.get();
+            System.out.println(response.body());
+            try {
+                JSONObject json = new JSONObject(response.body());
+                token = json.get("accessToken").toString();
+                System.out.println(token);
+                if (!token.equals(""))
+                    return true;
+            }catch (Exception e){ e.printStackTrace(); }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+
+
+    }
+
+    public void getDev(){
+        String endpoint = apiUrl + "/device";
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endpoint))
+                .GET()
+                .header("Authorization", "Bearer " + token)
+                .build();
+
+        CompletableFuture<HttpResponse<String>> futureResponse
+                = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+        try {
+            HttpResponse<String> response = futureResponse.get();
+            System.out.println(response.body());
+            /*try {
+                JSONObject json = new JSONObject(response.body());
+                //token = json.get("accessToken").toString();
+
+            }catch (Exception e){ e.printStackTrace(); }
+*/
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
 }
+
