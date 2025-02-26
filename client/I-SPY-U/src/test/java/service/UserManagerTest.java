d0537f8e156d1c035210c3e727cb7f566beaf07f
woues1 <toni.hirvikallio@gmail.com>
2025-02-26 19:29:11 +0200

    test

--- client/I-SPY-U/src/test/java/service/UserManagerTest.java
+++ client/I-SPY-U/src/test/java/service/UserManagerTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserManagerTest {
+    private static UserManager userManager = new UserManager();
 
     @Test
     void login() {
+
     }
 
     @Test
