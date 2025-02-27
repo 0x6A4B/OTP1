6439457dff9c8bcd32ed3e9e53fb47b4fa5c7d40
EinoRuuth <eino@ruuth.net>
2025-02-27 15:08:10 +0200

    added bindings to buttons in device settings

--- client/I-SPY-U/src/main/java/controller/DeviceController.java
+++ client/I-SPY-U/src/main/java/controller/DeviceController.java
@@ -6,9 +6,13 @@ import java.util.List;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.SingleSelectionModel;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import model.Device;
@@ -25,16 +29,24 @@ public class DeviceController extends IController {
 
     @FXML private TextField limitMin;
     @FXML private TextField limitMax;
+    @FXML private Button setLimitsButton;
 
     @FXML private TextField sharingEmail;
+    @FXML private Button shareButton;
 
     @FXML private Label actionItemLabel;
     @FXML private TextField actionInput;
+    @FXML private Button setActionButton;
 
     @FXML ChoiceBox<String> actionChoice;
+
     @FXML ChoiceBox<String> shareChoice;
+    @FXML private Button setShareButton;
 
     @FXML LineChart<String, String> lineChart;
+
+    private SingleSelectionModel<Tab> selectionModel;
+    @FXML TabPane tabPane;
 
     private User user;
     private Device device;
@@ -57,12 +69,14 @@ public class DeviceController extends IController {
         System.out.println(limitMax.getText());
         /* TODO tässä laitetaan limitit eteenpäin */
         System.out.println("set Limits");
+        selectionModel.select(0);
     }
 
     @FXML
     private void handleActionChoice() {
         System.out.println(actionChoice.getValue());
-        actionItemLabel.setText(actionChoice.getValue()+" for action");
+        String actionString = actionChoice.getValue();
+        actionItemLabel.setText(actionString.substring(7, actionString.length())+" for action");
         System.out.println("set Action choice");
     }
 
@@ -72,6 +86,7 @@ public class DeviceController extends IController {
         System.out.println(actionInput.getText());
         /* TODO tässä laitetaan action eteenpäin */
         System.out.println("set Action Happening");
+        selectionModel.select(0);
     }
 
     @FXML
@@ -79,6 +94,7 @@ public class DeviceController extends IController {
         System.out.println(sharingEmail.getText());
         /* TODO tässä laitetaan sharing eteenpäin */
         System.out.println("sharing Happening");
+        selectionModel.select(0);
     }
 
     @FXML
@@ -86,6 +102,7 @@ public class DeviceController extends IController {
         System.out.println(shareChoice.getValue());
         /* TODO tässä laitetaan sharing settings eteenpäin */
         System.out.println("sharing settings Happening");
+        selectionModel.select(0);
     }
 
     @Override
@@ -97,9 +114,13 @@ public class DeviceController extends IController {
         radioWeekly.setToggleGroup(toggleGroup);
         radioMontly.setToggleGroup(toggleGroup);
 
+        setLimitsButton.disableProperty().bind(limitMin.textProperty().isEmpty().or(limitMax.textProperty().isEmpty()));
+        shareButton.disableProperty().bind(sharingEmail.textProperty().isEmpty());
+
         actionChoice.setValue("Select an action");
         shareChoice.setValue("Select a role");
 
+        selectionModel = tabPane.getSelectionModel();
         setUpCharts();
     }
 }
