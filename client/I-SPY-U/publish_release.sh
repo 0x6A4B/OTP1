9571dfac1f50109ac93f8600f02811b20371a523
0x6A4B <0x6a4b@proton.me>
2025-04-01 15:58:56 +0300

    release body from last commit => puul request from sprint to main

--- client/I-SPY-U/publish_release.sh
+++ client/I-SPY-U/publish_release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+RELEASE_BODY=$(git log -1 | tail -n 1 | awk '{gsub(/  /, "", $0); print}')
 
 RELEASE_ID=$(curl -i -L \
     -X POST \
@@ -8,7 +9,7 @@ RELEASE_ID=$(curl -i -L \
     -H "Authorization: Bearer $GITHUB_TOKEN" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     https://api.github.com/repos/0x6A4B/OTP1/releases \
-    -d '{"tag_name":"'"$(date +%Y%m%d-%H%M)"'","target_commitish":"main","name":"I-SPY-U Client","body":"I-SPY-U","draft":false,"prerelease":false,"generate_release_notes":false}' \
+    -d '{"tag_name":"'"$(date +%Y%m%d-%H%M)"'","target_commitish":"main","name":"I-SPY-U Client","body":"'"$RELEASE_BODY"'","draft":false,"prerelease":false,"generate_release_notes":false}' \
     | grep id | head -n2 | tail -n1 | awk '{print substr($2, 1, length($2-1))}') &&
 
 
